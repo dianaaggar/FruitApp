37c87a683640be3fd3099a4defd0ca690461210b
dianaaggar <57739072+dianaaggar@users.noreply.github.com>
2023-08-11 00:25:09 -0400

    update

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 const express = require("express");
-
 const app = express();
-
-const PORT= 3005
+const PORT= 5000
 const fruits = require('./models/fruits');
 
 //-----------middleware----
+
 app.set("view engine", "jsx");
 
+app.engine("jsx", require("express-react-views").createEngine());
+
+
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
 
 
 app.get('/fruits', (req, res)=>{
-    res.send(fruits);
+    res.render("Index",{fruits: fruits});
 });
 
-//------- index => all fruit--
+
+app.post('/fruits', (req, res)=>{
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true; //do some data correction
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false; //do some data correction
+    }
+    fruits.push(req.body);
+    console.log(fruits);
+    res.send('data received');
+});
+
+
+
 
 app.get('/fruits/:indexOfFruitsArray', (req,res)=>{
-    res.send(fruits[req.params.indexOfFruitsArray]);
-});
+    res.render('Show', {
+        fruit: fruits[req.params.indexOfFruitsArray]
+    })
+})
 
 // ---show each fruit----------
 
+
+
+
 app.listen(PORT, (req, res)=>{
-    console.log(`listening on port ${PORT}`);
+    console.log(`listening on port ${PORT}`)
 });
 
 //-----server
